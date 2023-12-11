6f62dea08c47136e0259cfe083125cc4fe33318b
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2023-12-11 13:56:27 +0100

    redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Navbar } from '@/components'
 import './globals.css'
-
+import { Provider } from 'react-redux';
+import store from '@/store'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Provider store={store}>
+          {children}
+        </Provider>
       </body>
     </html>
   )
